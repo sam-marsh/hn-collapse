6000f535e9030dda88e213476320356d7a9c0eac
Sam <samuel.marsh@bigpond.com>
2016-04-08 12:56:21 +0100

    add files

---
+++ hn-collapse.safariextension/collapse.js
@@ -0,0 +1,53 @@
+$(document).ready(function() {
+
+    $('.comment-tree >> .athing').each(function() {
+
+        var thing = $(this);
+
+        var spn = $('<span></span>');
+        var btn = $('<a>[-]</a>');
+        spn.append(btn);
+
+        var hide = function() {
+            thing.find('.comment').hide();
+            btn.text('[+]');
+
+            var myIndent = thing.find("img[src='s.gif']").attr('width');
+
+            var children = [];
+            var curr = thing.next();
+            while (true) {
+                var currIndent = curr.find("img[src='s.gif']").attr('width');
+                if (currIndent > myIndent) {
+                    children.push(curr);
+                } else {
+                    break;
+                }
+                curr = curr.next();
+            }
+
+            for (var i = 0; i < children.length; ++i) {
+                children[i].hide();
+            }
+
+            btn.unbind().click(function() {
+                thing.find('.comment').show();
+
+                for (var i = 0; i < children.length; ++i) {
+                    children[i].show();
+                }
+
+                btn.text('[-]');
+                btn.unbind().click(hide);
+            });
+        };
+
+        btn.click(hide);
+
+        $(this).find('.comhead').each(function() {
+            $(this).append(spn);
+        });
+
+    });
+
+});
